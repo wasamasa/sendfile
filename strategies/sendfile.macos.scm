c199307a843cb5ac183b7b3bef48e0ed50f6d9eb
Jim Ursetto <jim@3e8.org>
2011-11-17 12:51:16 -0600

    Fix OS X sendfile corruption when EAGAIN occurs with partial data

--- strategies/sendfile.macos.scm
+++ strategies/sendfile.macos.scm
@@ -3,10 +3,15 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <errno.h>")
+
+;; EAGAIN may be signaled even when partial data is sent, but the caller expects EAGAIN
+;; to mean zero bytes sent, so we return the number of bytes sent when non-zero.
 (define %sendfile-implementation
   (foreign-lambda* double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
     "off_t res = (off_t)to_send;"
     "if(sendfile(src,dst,(off_t)offset,&res,NULL/*&hdtr*/,0) < 0){"
-    "  C_return(errno == EAGAIN ? -2 : -1);"
+    "  if(errno == EAGAIN)"
+    "    C_return(res == 0 ? -2 : (double)(offset + res));"
+    "  else C_return(-1);"
     "}"
     "C_return((double)(offset + res));"))
