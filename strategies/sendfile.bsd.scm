5c7149a7bc51ca9f2f6ad8c20ab6ae8f0de7a1e5
David Krentzlin <devnull@localhost>
2011-11-19 07:40:49 +0100

    fixed offset bug for bsd

--- strategies/sendfile.bsd.scm
+++ strategies/sendfile.bsd.scm
@@ -10,20 +10,28 @@
        "  C_return(-1);"
        "}"
        "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
-       "  C_return(errno == EAGAIN ? -2 : -1);"
+       "  if(errno == EAGAIN){"
+       "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
+       "  }else{"
+       "    C_return(-1);"
+       "  }"
        "}"
        " if(((off_t)offset & ~(READ_AHEAD - 1)) != (((off_t)offset + res) & ~(READ_AHEAD - 1))){"
        "  if(posix_fadvise(src,((off_t)offset + res) & ~(READ_AHEAD - 1),READ_AHEAD,POSIX_FADV_NOREUSE) < 0){"
        "    C_return(-1)"
        "  }"
        "}"
-       "C_return((double)(rtmpoffset + res));")))
+       "C_return((double)(tmpoffset + res));")))
   (else
    (define %sendfile-implementation
      (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
        "size_t res = 0;"
        "off_t tmpoffset = (off_t)offset;"
        "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
-       "  C_return(errno == EAGAIN ? -2 : -1);"
+       "  if(errno == EAGAIN){"
+       "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
+       "  }else{"
+       "    C_return(-1);"
+       "  }"
        "}"
        "C_return((double)(tmpoffset + res));"))))
