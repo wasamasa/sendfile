167c083e2350e340aeb59e6159e3249a413cb0ae
David Krentzlin <devnull@localhost>
2011-11-26 16:12:12 +0100

    replaced old testing code with the overhauled one

--- tests/run.scm
+++ tests/run.scm
@@ -1,288 +1,129 @@
-(use test tcp posix (srfi 4))
-(load "test-utils")
+;; 
+;; %%HEADER%%
+;; 
+
+(use test)
+(load "test-helper")
 (use sendfile)
-(import sendfile)
-
-(define the-server (setup))
-
-(test-begin "Chunking")
-
-(when sendfile-available
-  (test-group "sendfile"
-              (test "giving offsets"
-                    "ent (unofficial) maintainer of Scheme->C\n"
-                    (begin
-                      (with-prepared-environment
-                       offset-test-file
-                       (lambda (in out)
-                         (parameterize ((force-implementation 'sendfile))
-                           (sendfile in out offset: 1400))) #t)
-                      (file-contents test-file-out)))
-            
-              (test "giving size"
-                    " Scheme occupies a unique niche." 
-                    (begin
-                      (with-prepared-environment
-                       offset-test-file
-                       (lambda (in out)
-                         (parameterize ((force-implementation 'sendfile))
-                           (sendfile in out bytes: 32))) #t)
-                      (file-contents test-file-out)))
-            
-              (test "giving size and offset"
-                    "It is a mindset" 
-                    (begin
-                      (with-prepared-environment
-                       offset-test-file
-                       (lambda (in out)
-                         (parameterize ((force-implementation 'sendfile))
-                           (sendfile in out offset: 213 bytes: 15))) #t)
-                      (file-contents test-file-out)))
-
-            
-              ;; (test "giving too high offset" #t #f)
-              ;; (test "giving too low offset" #t #f)
-              ;; (test "giving too high size" #t #f)
-              ;; (test "giving too low size" #t #f)
-              ))
 
 
-(when mmap-available
- (test-group "mmap"
-             (test "giving offsets"
-                   "ent (unofficial) maintainer of Scheme->C\n"
-                   (begin
-                     (with-prepared-environment
-                      offset-test-file
-                      (lambda (in out)
-                        (parameterize ((force-implementation 'mmapped))
-                          (sendfile in out offset: 1400))) #t)
-                     (file-contents test-file-out)))
-             (test "giving size"
-                   " Scheme occupies a unique niche." 
-                   (begin
-                     (with-prepared-environment
-                      offset-test-file
-                      (lambda (in out)
-                        (parameterize ((force-implementation 'mmapped))
-                          (sendfile in out bytes: 32))) #t)
-                     (file-contents test-file-out)))
-            
-             (test "giving size and offset"
-                   "It is a mindset" 
-                   (begin
-                     (with-prepared-environment
-                      offset-test-file
-                      (lambda (in out)
-                        (parameterize ((force-implementation 'mmapped))
-                          (sendfile in out offset: 213 bytes: 15))) #t)
-                     (file-contents test-file-out)))
+(with-running-server
 
-             ;; (test "giving offset > page-size")
-             ;; (test "giving offset > page-size and size > page-size)
-            
-             ;; (test "giving too high offset" #t #f)
-             ;; (test "giving too low offset" #t #f)
-             ;; (test "giving too high size" #t #f)
-             ;; (test "giving too low size" #t #f)
-             ))
-
-(test-group "read-write-loop/fd"
-            (test "giving offsets"
-                  "ent (unofficial) maintainer of Scheme->C\n"
-                  (begin
-                    (with-prepared-environment
-                     offset-test-file
-                     (lambda (in out)
-                       (parameterize ((force-implementation 'read-write))
-                         (sendfile in out offset: 1400))) #t)
-                    (file-contents test-file-out)))
-
-            (test "giving size"
-                  " Scheme occupies a unique niche." 
-                  (begin
-                    (with-prepared-environment
-                     offset-test-file
-                     (lambda (in out)
-                       (parameterize ((force-implementation 'read-write))
-                         (sendfile in out bytes: 32))) #t)
-                    (file-contents test-file-out)))
-
-            (test "giving size and offset"
-                  "It is a mindset" 
-                  (begin
-                    (with-prepared-environment
-                     offset-test-file
-                     (lambda (in out)
-                       (parameterize ((force-implementation 'read-write))
-                         (sendfile in out offset: 213 bytes: 15))) #t)
-                    (file-contents test-file-out)))
-
-            ;; test non-page-aligned offsets
-            ;; test bigger chunks
-            
-            ;; (test "giving too high offset" #t #f)
-            ;; (test "giving too low offset" #t #f)
-            ;; (test "giving too high size" #t #f)
-            ;; (test "giving too low size" #t #f)
-            
-            )
+ (let* ((mb-buffer (generate-buffer (mebibytes 1)))
+        (mb-checksum (buffer-checksum mb-buffer)))
 
 
-(test-group "read-write-loop/port"
-            (test "giving offsets"
-                  "ent (unofficial) maintainer of Scheme->C\n"
-                  (begin
-                    (with-prepared-environment
-                     offset-test-file
-                     (lambda (in out)
-                       (parameterize ((force-implementation 'read-write-port))
-                         (sendfile in out offset: 1400))) #t #f)
-                    (file-contents test-file-out)))
+   (define (stream-mb-buffer)
+     (call-with-temporary-file/checksum
+      mb-buffer
+      (lambda (temp-file _)
+        (stream-file temp-file sendfile))))
+ 
+   (test-group "sendfile main interface"
+               
+               (test "sendfile" mb-checksum (stream-mb-buffer)))
 
-            (test "giving size"
-                  " Scheme occupies a unique niche." 
-                  (begin
-                    (with-prepared-environment
-                     offset-test-file
-                     (lambda (in out)
-                       (parameterize ((force-implementation 'read-write-port))
-                         (sendfile in out bytes: 32))) #t #f)
-                    (file-contents test-file-out)))
+   (test-group "forcing implementation"
+               
+               (parameterize ((force-implementation 'read-write))
+                 (test "read-write" mb-checksum (stream-mb-buffer)))
 
-            (test "giving size and offset"
-                  "It is a mindset" 
-                  (begin
-                    (with-prepared-environment
-                     offset-test-file
-                     (lambda (in out)
-                       (parameterize ((force-implementation 'read-write-port))
-                         (sendfile in out offset: 213 bytes: 15))) #t #f)
-                    (file-contents test-file-out)))
+               (if sendfile-available
+                   (parameterize ((force-implementation 'sendfile))
+                     (test "sendfile(2)" mb-checksum (stream-mb-buffer))))
 
-            
-            
-            ;; (test "giving too high offset" #t #f)
-            ;; (test "giving too low offset" #t #f)
-            ;; (test "giving too high size" #t #f)
-            ;; (test "giving too low size" #t #f)
+               (if mmap-available
+                   (parameterize ((force-implementation 'mmapped))
+                     (test "mmap(2)" mb-checksum (stream-mb-buffer))))
 
-            )
-
-(test-end "Chunking")
-
-(test-group "read-write-loop"
-      (test "send"
-       test-file-size
-       (with-prepared-environment test-file
-        (lambda (in out)
-          (impl:read-write-loop/fd in out 0 test-file-size)) #f #f))
-      (sleep 1)
-      (test "verify"
-       test-file-checksum
-       (compute-file-checksum test-file-out)))
-
-(test-group "read-write-loop (ports-only)"
-      (test "send"
-        test-file-size
-        (with-prepared-environment test-file
-         (lambda (in out)
-             (impl:read-write-loop/port in out 0 test-file-size)) #t #f))
-      (sleep 1)
-      (test "verify"
-       test-file-checksum
-       (compute-file-checksum test-file-out)))
+               (parameterize ((force-implementation 'read-write-port))
+                 (test "read-write-port" mb-checksum (stream-mb-buffer))))
 
 
-(if sendfile-available
-    (test-group "sendfile-impl"
-                (test "send"
-                      test-file-size 
-                      (with-prepared-environment test-file
-                       (lambda (in out)
-                         (impl:sendfile in out 0 test-file-size)) #f #f))
-                 (sleep 1)
-                 (test "verify"
-                       test-file-checksum
-                       (compute-file-checksum test-file-out))))
-
-(if mmap-available
-    (test-group "mmapped io"
-
-                (test "send"
-                      test-file-size 
-                      (with-prepared-environment test-file
-                       (lambda (in out)
-                         (impl:mmapped in out 0 test-file-size)) #f #f))
-                (sleep 1)
-                (test "verify"
-                      test-file-checksum
-                      (compute-file-checksum test-file-out))))
-                          
+   (test-group "read-write variations"
+               
+               (call-with-temporary-file/checksum
+                (generate-buffer (mebibytes 1))
+                (lambda (temp-file expected-checksum)
+                  (test "ports only"
+                        expected-checksum
+                        (call-with-connection-to-server
+                         (lambda (server-in server-out)
+                           (write-content-size server-out (mebibytes 1))
+                           (call-with-input-file temp-file
+                             (lambda (file-in)
+                               (sendfile file-in server-out)))
+                           (read-checksum server-in)))))))
 
 
+   (test-group "content chunking"
+               
+      (let* ((head   (generate-buffer 20 #\a))
+             (middle (generate-buffer 20 #\b))
+             (tail   (generate-buffer 20 #\c))
+             (buffer (string-append head middle tail)))         
 
-(test-begin "interface")
+        (define (chunking-test proc) 
+         (call-with-connection-to-server
+          (lambda (server-in server-out)
+            (let ((input (open-input-string buffer)))
+              (write-content-size server-out -1)
+              (proc input server-out)
+              (close-output-port server-out)
+              (read-checksum server-in)))))
 
-(test-group "sendfile"
-      (test "send"
-            test-file-size
-            (with-prepared-environment test-file
-             (lambda (in out)
-               (sendfile in out)) #t #f))
-      (sleep 1)
-      (test "verify"
-            test-file-checksum
-            (compute-file-checksum test-file-out)))
+        (define-syntax test-chunking
+          (syntax-rules ()
+            ((_ implementation)
+             (test-group implementation
+               (parameterize ((force-implementation (string->symbol implementation)))          
+                 (test "offsets"
+                       (buffer-checksum tail)
+                       (chunking-test
+                        (lambda (input output)
+                          (sendfile input output offset: 40)))
+                       )
+                 (test "size"
+                       (buffer-checksum head)
+                       (chunking-test
+                        (lambda (input output)
+                          (sendfile input output bytes: 20))))
+             
+                 (test "size and offset"
+                       (buffer-checksum middle)
+                       (chunking-test
+                        (lambda (input output)
+                          (sendfile input output offset: 20 bytes: 20)))))))))
+        
+        (when sendfile-available
+          (test-chunking "sendfile"))
 
+        (when mmap-available
+          (test-chunking "mmapped"))
 
+        (test-chunking "read-write")
+        
+        (test-chunking "read-write-port")))
 
+   (test-group "bugs"               
+               (call-with-buffer/checksum
+                (kibibytes 1)
+                (lambda (buffer checksum)
+                  (test "custom input port without fd [bug #542]"
+                        checksum
+                        (call-with-connection-to-server
+                         (lambda (server-in server-out)
+                           (write-content-size server-out (kibibytes 1))
+                           (sendfile (open-input-string buffer) server-out)
+                           (read-checksum server-in))))))
+   
+    
+               (call-with-temporary-file/checksum
+                (generate-buffer (mebibytes 2))
+                (lambda (temp-file expected-checksum)
+                  (test "send files > 1 mebibyte"
+                        expected-checksum
+                        (stream-file temp-file sendfile)))))))
 
-(test-group "custom input port without fd [bug #542]"
-            (let ((test-content "I'm content from a custom port"))
-              (test "send"
-                    (string-length test-content)
-                    (with-prepared-environment test-file
-                                               (lambda (ignored out)
-                                                 (sendfile
-                                                  (open-input-string test-content)
-                                                  out)) #t #t))))
-
-
-
-(test-end "interface")
-
-(test-begin "forcing implementation")
-
-(test "read-write-loop"
-      'read-write-loop
-      (with-prepared-environment test-file
-                                 (lambda (in out)
-                                   (parameterize ((force-implementation 'read-write))
-                                     (sendfile in out)
-                                     *last-selected-implementation*)) #t))
-
-(if sendfile-available
-    (test "sendfile"
-          'sendfile
-          (with-prepared-environment test-file
-                                     (lambda (in out)
-                                       (parameterize ((force-implementation 'sendfile))
-                                         (sendfile in out)
-                                         *last-selected-implementation*)) #t)))
-(if mmap-available
-    (test "mmapped"
-          'mmapped
-          (with-prepared-environment test-file
-                                     (lambda (in out)
-                                       (parameterize ((force-implementation 'mmapped))
-                                         (sendfile in out)
-                                         *last-selected-implementation*)) #t)))
-
-(test-end "forcing implementation")
-
-
-
-(tear-down the-server)
-  
 (unless (zero? (test-failure-count)) (exit 1))
+
