a7a197361fd2b37714a08fa2172408b6d859d076
David Krentzlin <devnull@localhost>
2011-12-01 21:34:05 +0100

    temporarily disabled fadvices

--- strategies/sendfile.bsd.scm
+++ strategies/sendfile.bsd.scm
@@ -3,39 +3,52 @@
 #include<sys/types.h>
 #include <sys/uio.h>")
 
-(cond-expand
-  (posix-fadvise
-   (foreign-declare "#define READ_AHEAD (4 * 1024 * 1024)")
-   (define %sendfile-implementation
-     (foreign-lambda* double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
-       "size_t res = 0;"
-       "off_t tmpoffset = (off_t)offset;"
-       "if((posix_fadvise(src,0,0,POSIX_FADV_SEQUENTIAL) < 0) && errno != ENOSYS){"
-       "  C_return(-1);"
-       "}"
-       "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
-       "  if(errno == EAGAIN){"
-       "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
-       "  }else{"
-       "    C_return(-1);"
-       "  }"
-       "}"
-       " if(((off_t)offset & ~(READ_AHEAD - 1)) != (((off_t)offset + res) & ~(READ_AHEAD - 1))){"
-       "  if(posix_fadvise(src,((off_t)offset + res) & ~(READ_AHEAD - 1),READ_AHEAD,POSIX_FADV_NOREUSE) < 0){"
-       "    C_return(-1)"
-       "  }"
-       "}"
-       "C_return((double)(tmpoffset + res));")))
-  (else
-   (define %sendfile-implementation
-     (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
-       "size_t res = 0;"
-       "off_t tmpoffset = (off_t)offset;"
-       "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
-       "  if(errno == EAGAIN){"
-       "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
-       "  }else{"
-       "    C_return(-1);"
-       "  }"
-       "}"
-       "C_return((double)(tmpoffset + res));"))))
+;; (cond-expand
+;;   (posix-fadvise
+;;    (foreign-declare "#define READ_AHEAD (4 * 1024 * 1024)")
+;;    (define %sendfile-implementation
+;;      (foreign-lambda* double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
+;;        "size_t res = 0;"
+;;        "off_t tmpoffset = (off_t)offset;"
+;;        "if((posix_fadvise(src,0,0,POSIX_FADV_SEQUENTIAL) < 0) && errno != ENOSYS){"
+;;        "  C_return(-1);"
+;;        "}"
+;;        "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
+;;        "  if(errno == EAGAIN){"
+;;        "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
+;;        "  }else{"
+;;        "    C_return(-1);"
+;;        "  }"
+;;        "}"
+;;        " if(((off_t)offset & ~(READ_AHEAD - 1)) != (((off_t)offset + res) & ~(READ_AHEAD - 1))){"
+;;        "  if(posix_fadvise(src,((off_t)offset + res) & ~(READ_AHEAD - 1),READ_AHEAD,POSIX_FADV_NOREUSE) < 0){"
+;;        "    C_return(-1)"
+;;        "  }"
+;;        "}"
+;;        "C_return((double)(tmpoffset + res));")))
+;;   (else
+;;    (define %sendfile-implementation
+;;      (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
+;;        "size_t res = 0;"
+;;        "off_t tmpoffset = (off_t)offset;"
+;;        "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
+;;        "  if(errno == EAGAIN){"
+;;        "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
+;;        "  }else{"
+;;        "    C_return(-1);"
+;;        "  }"
+;;        "}"
+;;        "C_return((double)(tmpoffset + res));"))))
+
+(define %sendfile-implementation
+  (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
+    "size_t res = 0;"
+    "off_t tmpoffset = (off_t)offset;"
+    "if(sendfile(src,dst,tmpoffset,to_send,NULL,&res,0) < 0){"
+    "  if(errno == EAGAIN){"
+    "    C_return(res == 0 ? -2 : (double)(tmpoffset + res));"
+    "  }else{"
+    "    C_return(-1);"
+    "  }"
+    "}"
+    "C_return((double)(tmpoffset + res));"))
--- strategies/sendfile.linux.scm
+++ strategies/sendfile.linux.scm
@@ -2,38 +2,54 @@
 #include <sys/sendfile.h>
 #include <fcntl.h>
 #include<errno.h>")
-(cond-expand
-  (posix-fadvise
-   (foreign-declare "#define READ_AHEAD (4 * 1024 * 1024)")
-   (define %sendfile-implementation
-     (foreign-lambda* double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
-       "size_t res = 0;"
-       "off_t curoffset = (off_t)offset;"
-       "if((posix_fadvise(src,0,0,POSIX_FADV_SEQUENTIAL) < 0) && errno != ENOSYS){ C_return(-1); }"
-       "if(sendfile(dst,src,&curoffset,to_send) < 0){"
-       "   if(errno == EAGAIN || errno == EINTR){ "
-       "     C_return(curoffset == 0 ? -2 : (double)(curoffset)); "
-       "   }else{"
-       "     C_return(-1);"
-       "   }"
-       "}"
-       " if(((off_t)offset & ~(READ_AHEAD - 1)) != (((off_t)offset + res) & ~(READ_AHEAD - 1))){"
-       "  if(posix_fadvise(src,((off_t)offset + res) & ~(READ_AHEAD - 1),READ_AHEAD,POSIX_FADV_NOREUSE) < 0){"
-       "    C_return(-1);"
-       "  }"
-       "}"
-       "C_return((double)curoffset);")))
-  (else
-   (define %sendfile-implementation
-     (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
-       "off_t curoffset = (off_t)offset;"
-       "if(sendfile(dst,src,&curoffset,to_send) < 0){"
-       "   if(errno == EAGAIN || errno == EINTR){    "
-       "     C_return(curoffset == 0 ? -2 : (double)(curoffset)); "
-       "   }else{ "
-       "     C_return(-1);"
-       "   }"
-       "}"
-       "C_return((double)curoffset);"))))
+
+;; This is temporarily disable until we find a way to reliably
+;; detect if this is supported
+
+;; (cond-expand
+;;   (posix-fadvise
+;;    (foreign-declare "#define READ_AHEAD (4 * 1024 * 1024)")
+;;    (define %sendfile-implementation
+;;      (foreign-lambda* double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
+;;        "size_t res = 0;"
+;;        "off_t curoffset = (off_t)offset;"
+;;        "if((posix_fadvise(src,0,0,POSIX_FADV_SEQUENTIAL) < 0) && errno != ENOSYS){ C_return(-1); }"
+;;        "if(sendfile(dst,src,&curoffset,to_send) < 0){"
+;;        "   if(errno == EAGAIN || errno == EINTR){ "
+;;        "     C_return(curoffset == 0 ? -2 : (double)(curoffset)); "
+;;        "   }else{"
+;;        "     C_return(-1);"
+;;        "   }"
+;;        "}"
+;;        " if(((off_t)offset & ~(READ_AHEAD - 1)) != (((off_t)offset + res) & ~(READ_AHEAD - 1))){"
+;;        "  if(posix_fadvise(src,((off_t)offset + res) & ~(READ_AHEAD - 1),READ_AHEAD,POSIX_FADV_NOREUSE) < 0){"
+;;        "    C_return(-1);"
+;;        "  }"
+;;        "}"
+;;        "C_return((double)curoffset);")))
+;;   (else
+;;    (define %sendfile-implementation
+;;      (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
+;;        "off_t curoffset = (off_t)offset;"
+;;        "if(sendfile(dst,src,&curoffset,to_send) < 0){"
+;;        "   if(errno == EAGAIN || errno == EINTR){    "
+;;        "     C_return(curoffset == 0 ? -2 : (double)(curoffset)); "
+;;        "   }else{ "
+;;        "     C_return(-1);"
+;;        "   }"
+;;        "}"
+;;        "C_return((double)curoffset);"))))
+
+(define %sendfile-implementation
+  (foreign-lambda*  double ((integer src) (integer dst) (double offset) (unsigned-integer to_send))
+    "off_t curoffset = (off_t)offset;"
+    "if(sendfile(dst,src,&curoffset,to_send) < 0){"
+    "   if(errno == EAGAIN || errno == EINTR){    "
+    "     C_return(curoffset == 0 ? -2 : (double)(curoffset)); "
+    "   }else{ "
+    "     C_return(-1);"
+    "   }"
+    "}"
+    "C_return((double)curoffset);"))
 
 
